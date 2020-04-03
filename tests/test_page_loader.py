0c4ae368862a0aa462019dfabeae99aeb9021612
GreyGreyWolf <WolfDieAtTheEnd@yandex.ru>
2020-04-03 18:10:08 +0300

    Fix

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -3,6 +3,7 @@ import os
 import tempfile
 from page_loader import engine
 
+
 url = 'https://greygreywolf.github.io/python-project-lvl3/'
 expected_file = './tests/fixtures/greygreywolf-github-io-python-project-lvl3-.html'
 
@@ -21,18 +22,6 @@ def test_get_page1():
         full_path = os.path.join(resource_dir_name, name)
         engine.get_page(test_dir, url) 
         assert read(full_path) == read(expected_file)
-
-
-# def test_get_page():
-#     with tempfile.TemporaryDirectory() as test_dir:
-#         name = engine.formation_name(url)
-#         dir_name = name[:-5]
-#         resource_dir_name = os.path.join(test_dir, dir_name)
-#         full_path = os.path.join(resource_dir_name, name)
-#         engine.get_page(test_dir, url)
-#         with open(full_path) as file1, open(expected_file) as file2:
-#             for file1Line, file2Line in zip(file1, file2):
-#                 assert file1Line == file2Line
 
 
 def test_name_correct():
