e81961e96e73afa1d68ba6adbb6e14001a25a195
GreyGreyWolf <wolfdieattheend@yandex.ru>
2020-04-07 18:56:43 +0300

    Step2: In progress

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -7,15 +7,15 @@ from page_loader import engine
 url = 'https://greygreywolf.github.io/python-project-lvl3/'
 
 
-def test_formatiopn_name_page():
+def test_create_name_page():
     expected_name = 'greygreywolf-github-io-python-project-lvl3-.html'
-    recieved_name = engine.formation_name(url)
+    recieved_name = engine.create_name_page(url)
     assert expected_name == recieved_name
 
 
 def test_create_dir_page():
     with tempfile.TemporaryDirectory() as test_dir:
-        page_name = engine.formation_name_page(url)
+        page_name = engine.create_name_page(url)
         dir_name = page_name[:-5]
         resource_dir_name = os.path.join(test_dir, dir_name)
         engine.get_page(test_dir, url)
