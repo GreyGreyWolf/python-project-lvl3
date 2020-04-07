e1b28b81c5d10761912bc832a1eca6c7818bdac5
GreyGreyWolf <wolfdieattheend@yandex.ru>
2020-04-07 18:55:15 +0300

    Step2: In progress

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -12,7 +12,7 @@ def start():
     print('\nPage loading is complete'.upper())
 
 
-def formation_name_page(url):
+def create_name_page(url):
     unformatted_name = urllib.parse.urlparse(url)
     page_name = unformatted_name.netloc + unformatted_name.path
     if page_name.startswith('www'):
@@ -23,6 +23,13 @@ def formation_name_page(url):
     page_name = '{}.{}'.format(page_name, 'html')
     return page_name
 
+
+def create_name_file(path):
+    for sym in path:
+        if sym in " ?!/;:":
+            name_file = path.replace(sym, '-')
+    return name_file
+    
 
 def create_dir_page(output, page_name):
     dir_name = page_name[:-5]
@@ -41,20 +48,25 @@ def create_dir_file(full_path_page):
     return path_files
 
 
-# def change_page(file):
-#     with open(file, 'r') as editable_file:
+# def change_page(full_path_page, path_files):
+#     with open(full_path_page, 'r') as editable_file:
 #         contetnts = editable_file.read()
 #         soup = BeautifulSoup(contetnts, 'lxml')
 #         tags = soup.findAll(['link','script', 'img'])
 #         for tag in tags:
-#             if tag.
+#             if tag.find('src'):
+#                 full_path_files = os.path.join(path_files, create_name_file(?) )
+#                 with open(path_files, 'w') as new_file:
+#                     new_file.write()
+#                 tag.replace_with(create_name_file(tag.text)
 
 
 def get_page(output, url):
     request = requests.get(url)
     request.encoding
-    file_name = formation_name_page(url)
+    file_name = create_name_page(url)
     full_path_page = create_dir_page(output, file_name)
     path_files = create_dir_file(full_path_page)
     with open(full_path_page, 'wb') as html_file:
         html_file.write(request.content)
+    change_page(full_path_page, path_files)
