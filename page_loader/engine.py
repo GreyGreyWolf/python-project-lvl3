e3d2fa8cd87d119ab970064b49d886ab4416ace2
GreyGreyWolf <WolfDieAtTheEnd@yandex.ru>
2020-04-12 21:03:00 +0300

    Step2: in progress:add download files

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -9,58 +9,71 @@ def start():
     parser = init_argparser()
     args = parser.parse_args()
     get_page(args.output, args.url)
-    print('\nPage loading is complete\n'.upper())
+    print('')
+    print('Page loading is complete'.upper())
+    print('')
 
 
-def create_name_page(url):
-    unformatted_name = urllib.parse.urlparse(url)
-    page_name = unformatted_name.netloc + unformatted_name.path
-    if page_name.startswith('www'):
-        page_name = page_name[4:]
-    for sym in page_name:
-        if sym in " ?.!/;:":
-            page_name = page_name.replace(sym, '-')
-    page_name = '{}.{}'.format(page_name, 'html')
-    return page_name
+def create_name(url):
+    url = url.split('?')[0]
+    parsed_link = urllib.parse.urlparse(url) 
+    if not parsed_link.netloc:
+        for sym in url:
+            if sym in " ?!/;:" :
+                name = url.replace(sym, '-')
+    else:
+        if parsed_link.netloc.startswith('www'):
+            parsed_link.netloc = parsed_link.netloc[4:]
+        name = parsed_link.netloc + parsed_link.path
+        for sym in name:
+            if sym in " ?.!/;:":
+                name = name.replace(sym, '-')
+        name = '{}.{}'.format(name, 'html')
+    return name
 
 
-def create_name_file(html_link):
-    name_file = html_link
-    if name_file.startswith('/'):
-        name_file = html_link[1:]
-    for sym in name_file:
-        if sym in " ?!/;:":
-            name_file = html_link.replace(sym, '-')
-    return name_file
-    
-
-def create_dir_page(output, page_name):
+def create_dir(output, page_name):
     dir_name = page_name[:-5]
     resource_dir_name = os.path.join(output, dir_name)
     os.makedirs(resource_dir_name)
-    full_path_page = os.path.join(resource_dir_name, page_name)
-    return full_path_page
+    path_page = os.path.join(resource_dir_name, page_name)
+    path_files = path_page[:-5] + '_files'
+    os.makedirs(path_files)
+    return path_page, path_files
 
 
-def create_dir_file(full_path_page):
-    dir_files = full_path_page[:-5] + '_files'
-    os.makedirs(dir_files)
-    return dir_files
-
-
-# def change_page(html_file, dir_files):
-#     with open(html_file) as editable_file:
-#         soup = bs4.BeautifulSoup(editable_file, 'xml')
-#         for tag in soup.find_all(['link', 'script', 'img'])
-
+def write_content(url, path):
+    if not url.startswith('http'):
+        url =  ''.join(['https://', url])  
+    request = requests.get(url)
+    request.encoding
+    with open(path, 'wb') as new_file:
+        new_file.write(request.content)
+    return request
 
 
 def get_page(output, url):
-    request = requests.get(url)
-    request.encoding
-    page_name = create_name_page(url)
-    full_path_page = create_dir_page(output, page_name)
-    dir_files = create_dir_file(full_path_page)
-    with open(full_path_page, 'wb') as html_file:
-        html_file.write(request.content)
-    # change_page(full_path_page, dir_files)
+    list_tags = []
+    page_name = create_name(url)
+    path_page, dir_files = create_dir(output, page_name)
+    request = write_content(url, path_page)
+    soup = bs4.BeautifulSoup(request.text, 'lxml')
+    for tag in soup.find_all({ 'link': True, 'img': True, 'script': True}):
+        if tag.name == 'link':
+            parsed_link = urllib.parse.urlparse(tag['href'])
+            path = urllib.parse.urlparse(tag['href']).path
+            extn = os.path.splitext(path)[1]
+            if extn and not parsed_link.netloc:
+                list_tags.append(tag['href'])    
+        elif (tag.name == 'img' or 'script') and tag.has_attr('src'):
+            parsed_link = urllib.parse.urlparse(tag['src'])
+            path = urllib.parse.urlparse(tag['src']).path
+            extn = os.path.splitext(path)[1]
+            if extn and not parsed_link.netloc:
+                list_tags.append(tag['src']) 
+        for elem in list_tags:
+            path_files = os.path.join(dir_files, create_name(elem))
+            domain = urllib.parse.urlparse(url)
+            local_url = urllib.parse.urlunparse(domain._replace(path = elem))
+            write_content(local_url, path_files)
+  
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -9,13 +9,13 @@ url = 'https://greygreywolf.github.io/python-project-lvl3/'
 
 def test_create_name_page():
     expected_name = 'greygreywolf-github-io-python-project-lvl3-.html'
-    recieved_name = engine.create_name_page(url)
+    recieved_name = engine.create_name(url)
     assert expected_name == recieved_name
 
 
-def test_create_dir_page():
+def test_create_dir():
     with tempfile.TemporaryDirectory() as test_dir:
-        page_name = engine.create_name_page(url)
+        page_name = engine.create_name(url)
         dir_name = page_name[:-5]
         resource_dir_name = os.path.join(test_dir, dir_name)
         engine.get_page(test_dir, url)
@@ -24,7 +24,7 @@ def test_create_dir_page():
 
 def test_create_page():
     with tempfile.TemporaryDirectory() as test_dir:
-        page_name = engine.create_name_page(url)
+        page_name = engine.create_name(url)
         dir_name = page_name[:-5]
         resource_dir_name = os.path.join(test_dir, dir_name)
         full_created_path = os.path.join(resource_dir_name, page_name)
